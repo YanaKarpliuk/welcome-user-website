dae57f4fef3a12da5c1e4ccac3a95e799571bb53
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-01-07 23:47:40 +0200

    completed about section

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,7 @@ module.exports = {
       '@mainScreen': path.resolve(__dirname, 'src/components/MainScreen'),
       '@toolbar': path.resolve(__dirname, 'src/components/Toolbar'),
       '@icons': path.resolve(__dirname, 'src/images/icons'),
+      '@images': path.resolve(__dirname, 'src/images'),
     }
   },
 };
--- src/components/MyComputer/About.tsx
+++ src/components/MyComputer/About.tsx
@@ -1,13 +1,27 @@
+import ghost from "@images/ghost.gif";
+import Typewriter from "typewriter-effect";
+
 export default function About() {
   return (
     <section>
-      <h2>Hi there!</h2>
-      <p>My name is Yana and I am an aspiring software developer.</p>
-      <p>
-        I've built this website as a way to practice my skills and learn new
-        things.
-      </p>
-      <p>I hope you have fun playing the games I've added. Enjoy ;)</p>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .changeDelay(10)
+            .typeString(
+              "<span>Hi there! My name is Yana and I am an aspiring software developer.</span><br/>"
+            )
+            .typeString(
+              "<span>I've built this website as a way to practice my skills and learn new things.</span><br/>"
+            )
+            .typeString(
+              "<span>I hope you have fun playing the games I've added. Enjoy ;)</span>"
+            )
+            .start();
+        }}
+      />
+
+      <img src={ghost} alt="ghost gif" className="h-[200px] mx-auto" />
     </section>
   );
 }
