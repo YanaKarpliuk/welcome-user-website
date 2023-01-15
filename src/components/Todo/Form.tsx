2ab039227c00c09943fff8c1a0fcb8bdac10f4ef
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-01-15 16:45:17 +0200

    styled the form

--- src/components/Todo/Form.tsx
+++ src/components/Todo/Form.tsx
@@ -1,16 +1,26 @@
-export default function Form() {
+import { SyntheticEvent } from "react";
+
+type FormTypes = {
+  handleSubmit: (e: SyntheticEvent) => void;
+};
+
+export default function Form({ handleSubmit }: FormTypes) {
   return (
-    <section className="border-r-[1px] flex-1">
-      <form className="flex flex-col">
-        <label htmlFor="new-task-input">Enter a new task:</label>
+    <section className="border-r-[1px] flex-1 p-[20px]">
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label className="mb-[10px]" htmlFor="new-task-input">
+          Enter a new task:
+        </label>
         <input
           id="new-task-input"
-          className=""
+          className="mb-[20px] border-[1px] border-black rounded-[4px] p-[5px] outline-none focus:border-blueLink"
           type="text"
           placeholder="e.g Walk a dog"
           name="content"
         />
-        <input className="" type="submit" value="Add Task" />
+        <button className="bg-blueLink text-white rounded-[4px] p-[5px] cursor-pointer hover:shadow-lightHover transition-effect">
+          Add Task
+        </button>
       </form>
     </section>
   );
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,13 +1,17 @@
 import Form from "components/Todo/Form";
 import Tasks from "components/Todo/Tasks";
 import TopSection from "components/TopSection/TopSection";
+import { SyntheticEvent } from "react";
 
 export default function Todo() {
+  function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault()
+  }
   return (
     <section className="h-[100%] flex flex-col">
       <TopSection title="To do" />
       <div className="text-[24px] flex h-[100%]">
-        <Form />
+        <Form handleSubmit={handleSubmit} />
         <Tasks />
       </div>
     </section>
